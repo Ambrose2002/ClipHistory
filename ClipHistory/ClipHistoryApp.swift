43cda760fb88be101579d0cfb30c9601d3cd03c4
Ambrose Blay <ab2838@cornell.edu>
2026-01-07 11:29:30 -0500

    Initial Commit

---
+++ ClipHistory/ClipHistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClipHistoryApp.swift
+//  ClipHistory
+//
+//  Created by Ambrose Blay on 1/7/26.
+//
+
+import SwiftUI
+
+@main
+struct ClipHistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
