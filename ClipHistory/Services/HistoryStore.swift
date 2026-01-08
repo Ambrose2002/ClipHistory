1b3d9c04fefc93c22c09e5a45024471b156d393e
Ambrose Blay <ab2838@cornell.edu>
2026-01-08 18:35:59 -0500

    feat: add persistence for clipboard history
    Introduce writeClip and loadClips helpers to store clipboard items
    as JSON in the documents directory and load them back safely

--- ClipHistory/Services/HistoryStore.swift
+++ ClipHistory/Services/HistoryStore.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+
+
+func writeClip(clip: ClipboardItem) {
+    
+    let fileURL = URL.documentsDirectory.appending(path: "clips.json")
+    
+    do {
+        var current : [ClipboardItem] = loadClips();
+        current.append(clip)
+        
+        let data = try JSONEncoder().encode(current)
+        
+        try data.write(to: fileURL, options: [.atomic, .completeFileProtection])
+    } catch {
+        print("Error saving JSON: \(error)")
+    }
+}
+
+
+func loadClips() -> [ClipboardItem] {
+    let fileURL = URL.documentsDirectory.appending(path: "users.json")
+    
+    // Check if file exists first
+    guard FileManager.default.fileExists(atPath: fileURL.path) else { return [] }
+    
+    do {
+
+        let data = try Data(contentsOf: fileURL)
+        
+        // Decode JSON data back into Swift objects
+        let clips = try JSONDecoder().decode([ClipboardItem].self, from: data)
+        return clips
+    } catch {
+        print("Error loading JSON: \(error)")
+        return []
+    }
+}
